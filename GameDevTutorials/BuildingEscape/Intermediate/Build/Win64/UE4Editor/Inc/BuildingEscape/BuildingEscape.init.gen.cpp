79defba0e51864511ff3f303f10e76881bd52ea0
Theodor Risager <theodor349@gmail.com>
2021-02-03 20:38:45 +0100

    Created WorldPosition Component

--- GameDevTutorials/BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
+++ GameDevTutorials/BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeBuildingEscape_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x52AA0BA1,
-				0xAC96D55F,
+				0x034CDC27,
+				0xF5022432,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
