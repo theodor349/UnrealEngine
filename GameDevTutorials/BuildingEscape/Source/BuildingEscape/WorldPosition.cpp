e592958e3f2ba1409f706fae997b610658af3881
Theodor Risager <theodor349@gmail.com>
2021-02-05 14:13:00 +0100

    EP BSP Challenge

--- GameDevTutorials/BuildingEscape/Source/BuildingEscape/WorldPosition.cpp
+++ GameDevTutorials/BuildingEscape/Source/BuildingEscape/WorldPosition.cpp
@@ -2,6 +2,7 @@
 
 
 #include "WorldPosition.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -19,8 +20,11 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	AActor* owner = GetOwner();
+	FString name = owner->GetName();
+	FVector pos = owner->GetTransform().GetLocation();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is located at %s"), *name, *pos.ToString());
 }
 
 
