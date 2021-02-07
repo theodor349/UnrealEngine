7f71931fd4585d0b9aa8975b3bc2493e1974900a
Theodor Risager <theodor349@gmail.com>
2021-02-07 12:51:54 +0100

    Turret Check Timer

--- GameDevTutorials/ToonTanks/Source/ToonTanks/Pawns/PawnTank.h
+++ GameDevTutorials/ToonTanks/Source/ToonTanks/Pawns/PawnTank.h
@@ -16,20 +16,24 @@ class TOONTANKS_API APawnTank : public APawnBase
 	GENERATED_BODY()
 
 private:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess="true"))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Movement", meta = (AllowPrivateAccess="true"))
 	USpringArmComponent* SpringArm;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess="true"))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Movement", meta = (AllowPrivateAccess="true"))
 	UCameraComponent* Camera;
 
+	UPROPERTY(VisibleInstanceOnly, Category="Movement")
 	FVector MoveDirection;
+	UPROPERTY(VisibleInstanceOnly, Category="Movement")
 	FQuat RotationDirection;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement", meta = (AllowPrivateAccess="true"))
+	float MoveSpeed = 100.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement", meta = (AllowPrivateAccess="true"))
+	float RotationSpeed = 100.f;
 
 	void CalculateMoveInput(float Value);
 	void CalculateRotateInput(float Value);
 
-	float MoveSpeed = 100.f;
-	float RotationSpeed = 100.f;
-	
 	void Move();
 	void Rotate();
 	
